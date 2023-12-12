94f8b44e0028ac9e32b01585d21a9af600373e5d
Sorawit yoktlemtae <Sorawit_yok@cmu.ac.th>
2023-12-12 16:06:34 +0700

    lab8 com

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,38 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+
+int main(){
+  string chars;
+  int age,height,bounty;
+  cout << "Enter your age: ";
+  cin >> age;
+  if(age <= 25){
+     cout << "Enter your height: ";
+     cin >> height;
+     if(height < 100){
+        chars = "Chopper";
+      }else if(height < 180){
+        chars = "Usopp";
+      }else{
+
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 1100*pow(10,6)){
+        chars = "Zoro";
+      }else {chars = "Sanji";}
+    }
+
+  }else if(age <= 60){
+     cout << "Enter your bounty: ";
+     cin >> bounty;
+    if(bounty > 500*pow(10,6)){
+        chars = "Jinbe";
+    }else{  
+        chars = "Franky";
+    }
+  }else {chars = "Brook";}
+  cout << "Your character = " << chars;
+}
+//0000
